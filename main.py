33ee0949b0380cdfa8b004f6bb3661d9cf6d00f4
muzape28-blip <muzape28@gmail.com>
2026-07-20 08:22:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -194,4 +194,4 @@ PROVIDER_HANDLERS = {
 
 if __name__ == "__main__":
     # Pake waitress server untuk WebView Android agar thread tidak freeze/force close
-    serve(app, host="127.0.0.1", port=5000)
+    serve(app, host="127.0.0.1", port=8080)
